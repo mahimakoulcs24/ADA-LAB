910f1885ea478beafaddec03cf3f4349110c8df9
mahimakoulcs24 <mahimakoul.cs24@bmsce.ac.in>
2026-04-13 09:51:43 +0530

    Create code.c

---
+++ Fractional Knapsack/code.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+struct Item
+{
+    int value;
+    int weight;
+    float ratio;
+};
+
+void swap(struct Item *a, struct Item *b)
+{
+    struct Item temp= *a;
+    *a=*b;
+    *b= temp;
+}
+
+void sortItems(struct Item arr[], int n)
+{
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j].ratio < arr[j+1].ratio){
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+float fractionalKnapsack(int capacity, struct Item arr[], int n)
+{
+    float totalValue= 0.0;
+    for(int i=0; i<n; i++){
+        if(capacity>= arr[i].weight){
+            capacity -= arr[i].weight;
+            totalValue += arr[i].value;
+        }
+        else{
+            totalValue += arr[i].value* (float)capacity/arr[i].weight;
+            break;
+        }
+    }
+    return totalValue;
+}
+
+int main()
+{
+    int n, capacity;
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+
+    struct Item arr[n];
+    printf("\nEnter the value and weight of each item: ");
+    for(int i=0; i<n; i++){
+        scanf("%d%d", &arr[i].value, &arr[i].weight);
+        arr[i].ratio= (float)arr[i].value/arr[i].weight;
+    }
+    printf("\nEnter capacity of knapsack :");
+    scanf("%d", &capacity);
+
+    sortItems(arr, n);
+
+    float maxValue= fractionalKnapsack(capacity, arr , n);
+    printf("\nThe maximum value of knapsack is: %.2f\n", maxValue);
+    return 0;
+}
